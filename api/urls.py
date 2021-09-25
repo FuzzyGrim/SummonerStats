31fed744fd9c80dd52a7b698a01afba4dacc3506
FuzzyGrim <stromed_chariotman@aleeas.com>
2021-09-25 11:51:27 +0200

    todo in-game

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("<str:server>/<str:summoner_name>/", views.user_info),
     path("<str:server>/<str:summoner_name>/tmp/<str:gameId>/", views.getGameData),
-    path("<str:server>/<str:summoner_name>/In-Game/", views.in_game),
 ]
